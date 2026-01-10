fcfb11c043ba8943f0d0eeb9bb7ce69388f47a9c
3066aggrim <aryalaggrim@gmail.com>
2026-01-10 09:18:42 +0545

    typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+name = "aggrim"
+age = 15
+is_student = True
+weight = 36.5
+
+print("Name:", name)
+print("data type of name is", type(name))
+
+print("age:", type(age))
+print("data type of age is", type(age))
+
+print("is_student:", type(is_student))
+print("data type of is_student", type(is_student))
+print("weight:", type(weight))
+print("data type of weight is", type(weight))
+print("\n after type casting......")
+age = str(age)
+print(age)
+print("data type of age is", type(age))
+weight = int(weight)
+print("data type of weight is", type(weight))
