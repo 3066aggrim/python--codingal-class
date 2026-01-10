5c83ed6423c6dde0ff48d18b33646aafd5df1fe4
3066aggrim <aryalaggrim@gmail.com>
2026-01-10 08:47:27 +0545

    variablesno2.py

---
+++ variablesno2.py
@@ -0,0 +1,10 @@
+Aayan = "2013/11/23"
+Aagam = "2012/9/8"
+Aaditya = "2014/2/23"
+Kalan = "2013/3/12"
+Riwaj = "2013/9/29"
+print("Aayan", Aayan)
+print("Aagam", Aagam)
+print("Aaditya", Aaditya)
+print("Kalan", Kalan)
+print("Riwaj", Riwaj)
