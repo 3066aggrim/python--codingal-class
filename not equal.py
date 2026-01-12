bf90290a7ff3a38278a2bc5df37c798b40fe47f0
3066aggrim <aryalaggrim@gmail.com>
2026-01-12 13:10:27 +0545

    not equal

---
+++ not equal.py
@@ -0,0 +1,19 @@
+a = 23
+b = 30
+c = 30
+d = 23
+
+print(a != b)
+print(b != c)
+print(a != b)
+
+a = "aggrim"
+b = "prazna"
+
+print(a != b)
+
+a = 22
+b = 66
+
+if a == b or a != b:
+    print("prazna")
