d87a964291f6ac867e257a920562157504bc2f90
3066aggrim <aryalaggrim@gmail.com>
2026-01-12 10:46:11 +0545

    temperature.py

---
+++ temperature.py
@@ -0,0 +1,22 @@
+temperature = int(input("enter the temperature"))
+if 1 <= temperature <= 5:
+    print("Wear heavy winter clothes, coat, gloves, and cap")
+    print("its to cold🥶!")
+
+if 5 <= temperature <= 10:
+    print("Cold 🌬️")
+    print("Wear a jacket or sweater")
+
+if 15 <= temperature <= 20:
+    print("Cool 🙂")
+    print("Wear a light sweater or hoodie")
+
+
+if 20 <= temperature <= 25:
+    print("Mild 🌤️")
+    print("Wear comfortable full-sleeve clothesand cap")
+
+
+if 25 <= temperature <= 30:
+    print("its hot weather🔆")
+    print("wear light clothes and stand in front of the fan")
