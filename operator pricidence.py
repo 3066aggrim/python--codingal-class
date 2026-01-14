adcb48dd26088690b376a30b789e98d0bf8ac7d5
3066aggrim <aryalaggrim@gmail.com>
2026-01-14 13:03:27 +0545

    operator pricidence

---
+++ operator pricidence.py
@@ -0,0 +1,16 @@
+a = 8
+b = 6
+c = 8
+d = 2
+
+y = (a+b+(c*b))/a+b
+print("y is", y)
+
+z = "Aggrim"
+y = 13
+
+if type(z) is float or type(z) is str:
+    print("hello")
+
+else:
+    print("welcome")
