fe4715876a70a51483418509f59059d2179aff60
3066aggrim <aryalaggrim@gmail.com>
2026-01-16 13:26:55 +0545

    reverse order

---
+++ module 2/Strimg reverse.py
@@ -0,0 +1,8 @@
+string1 = input("please enter a word")
+string2 = ''
+
+for i in string1:
+    string2 = i + string2
+
+print("\n THe original string is", string1)
+print("\n the reversed form is", string2)
---
+++ module 2/reverse order.py
@@ -0,0 +1,6 @@
+number = int(input("please enter a number"))
+
+print("numbers from{0}to{1} are".format(number, 1))
+
+for i in range(number, 0, -2):
+    print(i)
