b1021139b21b7c3dede18745c42f197d2b70081e
3066aggrim <aryalaggrim@gmail.com>
2026-03-28 08:28:56 +0545

    radius

---
+++ module_3/circumference.py
@@ -0,0 +1,8 @@
+def circumference(radius):
+    return 2*3.14*radius
+
+
+radius = float(input("Enter the radius of the circle in meter: "))
+x = circumference(radius)
+
+print("Circumference of a circle is:", x, "m")
