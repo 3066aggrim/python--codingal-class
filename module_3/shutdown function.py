d9a68647ebecf19ca01b11e231fb5743ed423e43
3066aggrim <aryalaggrim@gmail.com>
2026-03-29 13:26:46 +0545

    hi

---
+++ module_3/shutdown function.py
@@ -0,0 +1,14 @@
+def shutdown():
+    user_input = input("Do you want to shut down the system? (yes/no): ")
+
+    if user_input == "yes":
+        print("Shutting down...")
+
+    elif user_input == "no":
+        print("Abort shutting down.")
+
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+
+
+shutdown()
