a95603b903ac3b33ac277782cd4965707ba73cc5
3066aggrim <aryalaggrim@gmail.com>
2026-03-31 14:04:55 +0545

    hi

--- module_3/age_counter.py
+++ module_3/age_counter.py
@@ -1,7 +1,8 @@
-age = (int(input("please enter your age: ")))
+
 
 while True:
     try:
+        age = (int(input("please enter your age: ")))
         print("valid output")
 
         if age % 2 == 0:
@@ -14,9 +15,6 @@ while True:
 
     except ValueError:
         print("its a value error")
-
-    except ZeroDivisionError:
-        print("Zero Division Error")
 
     finally:
         print("Completed")
