6a05582f6c35cb7eb8eff3bbe2145e4733ffee06
3066aggrim <aryalaggrim@gmail.com>
2026-04-04 10:13:11 +0545

    hi

---
+++ module 1/bank.py
@@ -0,0 +1,7 @@
+sign_in = str(input("if you have a account please enter it in yes or no:"))
+sign_up = str("if you dont have a account then sign up")
+
+if sign_in == "yes":
+    sign_in = str(input("please enter your account:"))
+
+    if
---
+++ module 4/product_tupples.py
@@ -0,0 +1,7 @@
+num = (1, 3, 5, 4, 2, 6, 7)
+product = 1
+
+for i in range(0, 7):
+    product = num[i]*product
+
+print("the product of the given tuples are", product)
